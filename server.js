3e9f443645aa8f0d61ce29486718fb023dce4a2a
Dorin <comsaiuliand@gmail.com>
2020-09-02 22:56:46 +0300

    Commit 2/9/2020 Homepage + Register Frontend

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 var express = require('express')
 var server  = express()
 
+server.set('view engine','ejs')
+server.use(express.json())
 server.use(express.static('public'))
-
+server.use(logger)
 
 server.get('/',function(req,res){
     res.sendFile(__dirname + "/index.html")
+    
 })
 
+server.get('/register',(req,res)=>{
+    
+    res.render('register')
+})
 
 server.listen(80,function(){
     console.log("Listening on port 80")
 })
+
+
+function logger(req,res,next){
+    console.log("Request ["+req.method+"] made at [" + new Date().toLocaleTimeString() + "] by ["  + req.ip + "] for [" +req.url + "]")
+    next();
+}
