0511880bd9ba7d69ffa1d983ec0caf149c80cbf6
Dorin from home <comsaiuliand@gmail.com>
2020-10-27 22:00:11 +0200

    Setting tab
    Finishing setting tab on profile page and wiring it to the about page

--- public/scripts/postDialogs.js
+++ public/scripts/postDialogs.js
@@ -21,20 +21,27 @@ var deleteContent = document.getElementById('deleteContent')
 var confirmDelete = document.getElementById('confirmDelete')
 var cancelDelete = document.getElementById('cancelDelete')
 
-
+function initiatePost(){
+    overlay.style.display='flex'
+    document.body.style.overflowY='hidden'
+}
+function finalizePost(){
+    document.body.style.overflowY='auto'
+    overlay.style.display='none'
+}
 function initiatePostDelete(event){
+    initiatePost()
     var postID = event.attributes.postID.value;
     var content = event.parentElement.parentElement.parentElement.previousElementSibling.firstElementChild.innerText
 
-    overlay.style.display="flex"
     deleteContent.innerText=content
     confirmDelete.setAttribute('href','/deletepost/'+postID)
     deletePOPUP.style.display="block"
 }
 
 cancelDelete.addEventListener('click',function(){
-    overlay.style.display='none'
     deletePOPUP.style.display="none"
+    finalizePost()
 })
 
 //EDIT POST
@@ -45,29 +52,24 @@ var confirmEdit = document.getElementById('confirmEdit')
 var editForm = document.getElementById('editForm')
 
 async function initiatePostEdit(event){
+    initiatePost()
 
     var postID = event.attributes.postID.value;
     editForm.setAttribute('action', "/editpost/"+postID) 
-
     var data = await fetch('/getpostcontent/'+postID)
     var content = await data.json()
     
-    overlay.style.display='flex'
     editPOPUP.style.display='block'
-    document.body.style.overflowY='hidden'
-    document.body.style.height='100vh'
     editTextarea.value= content.content
     
     editTextarea.focus()
     editTextarea.setSelectionRange(editTextarea.value.length,editTextarea.value.length);
-
-    
 }
 confirmEdit.addEventListener('click',function(){
+    finalizePost()
     editForm.submit()
 })
 cancelEdit.addEventListener('click',function(){
-    document.body.style.overflowY='visible'
-    overlay.style.display='none'
+    finalizePost()
     editPOPUP.style.display="none"
 })
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -32,4 +32,24 @@ router.post('/user/:username/update',verify, async function(req,res){
     var findandupdate = await Mongo.db.db('typhoon').collection('users').findOneAndUpdate(query,update)
     res.status(200).redirect('back')
 })
+router.post('/user/:username/updatepreferences',verify,async function(req,res){
+    var logged_user = await Mongo.db.db('typhoon').collection('users').findOne({_id:ObjectId(req.userID)},{projection:{_id:0,password:0}})
+    
+    let preferences = {
+        email: req.body.email === 'true',
+        birthdate: req.body.birthdate === 'true',
+        phonenumber: req.body.phonenumber === 'true',
+        adress: req.body.adress === 'true',
+        occupation: req.body.occupation === 'true',
+        relationstatus: req.body.relationstatus === 'true'
+    } 
+
+    let query={_id:ObjectId(req.userID)}
+    let update = {$set:{preferences: preferences}}
+
+    let findandupdatepreferences = await Mongo.db.db('typhoon').collection('users').findOneAndUpdate(query,update)
+    res.status(2002).redirect('back')
+})
+
+
 module.exports= router
